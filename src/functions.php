e6d1dc96df6e8df1aece69571c8f7bc19bbf8eb0
Alex Perona <alpebe@floridauniversitaria.es>
2024-09-20 13:27:43 +0200

    Añadida función saludoPersonalizado en functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,9 @@
 <?php
-function saludo(): string {
+function saludo() {
     return '¡Hola desde la función saludo!';
 }
-?>"
+
+function saludoPersonalizado($nombre) {
+    return "¡Hola, $nombre! Bienvenido al proyecto.";
+}
+?>;
